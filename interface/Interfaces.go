1dcaf28507d20e4b9700483c74260b6ccedf299c
aandrade <alexander.andrade@lifemiles.com>
2021-04-17 09:45:40 -0600

    dejando todo con abstract factory

---
+++ interface/Interfaces.go
@@ -0,0 +1,9 @@
+package _interface
+
+type Operation interface{
+	Traslator(text string, language string) (string, error)
+}
+
+type Process interface{
+	GetProcessTraslator(text string, language string) (Operation,error)
+}
--- methods/interface.go
+++
@@ -1,7 +0,0 @@
-package methods
-
-type operations interface{
-	ConvertToBinary(text string,origin string) (string)
-	ConvertToText(text string,origin string) (string)
-	ConvertToMorse(text string, origin string) (string)
-}
